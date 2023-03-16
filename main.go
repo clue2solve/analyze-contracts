9539820ceed2e4c439d87550fe978b7b799dc2c7
honnuanand <anand@honnu.com>
2023-03-16 10:12:46 -0700

    adding the parser

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "github.com/honnuanand/analyze-contracts/contractparser"
+import "github.com/username/analyze-contracts/contractparser"
+
 
 func main() {
-	contractparser.ParseContract()
+	sourceCode := `
+        pragma solidity ^0.8.0;
+
+        contract MyContract {
+            uint public myNumber;
+            uint[] public myArray;
+            mapping(address => uint) public myMap;
+
+            constructor(uint _myNumber) {
+                myNumber = _myNumber;
+            }
+
+            function setNumber(uint _myNumber) public {
+                myNumber = _myNumber;
+            }
+
+            function setArray(uint[] memory _myArray) public {
+                myArray = _myArray;
+            }
+
+            function setMap(address _address, uint _value) public {
+                myMap[_address] = _value;
+            }
+        }
+    `
+
+	parser, err := NewContractParser(sourceCode)
+	if err != nil {
+		panic(err)
+	}
+
+	parser.ParseVariables()
 }
